453cdea130220b2f1ad136e73c59fa1a638285e1
unknown <shashank.thapa.mgr@gmail.com>
2020-09-08 12:56:08 +0545

    Countdown timer

---
+++ Countdown Timer/app/script/script.js
@@ -0,0 +1,36 @@
+let daysEl = document.getElementById('days');
+let hoursEl = document.getElementById('hours');
+let minsEl = document.getElementById('mins');
+let secondsEl = document.getElementById('seconds');
+
+let finalDate = "1 Jan 2021";
+
+function calculateDate(){
+    let releaseDate = new Date(finalDate);
+    let currentDate = new Date();
+
+    const totalSeconds = (releaseDate - currentDate) / 1000;
+
+    const days = Math.floor(totalSeconds / 86400);
+
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+
+    const mins = Math.floor(totalSeconds / 60) % 60;
+
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    console.log(days, hours, mins, seconds);
+
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = formatTime(hours);
+    minsEl.innerHTML = formatTime(mins);
+    secondsEl.innerHTML = formatTime(seconds);
+
+}
+
+function formatTime(time){
+    return time < 10 ? `0${time}` : time;
+
+}
+
+setInterval(calculateDate, 1);
